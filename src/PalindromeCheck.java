1619302297a50ae29c2188728ad18f8128d14043
[bsaimukeshreddy] <bsaimukeshreddy143@gmail.com>
2022-09-26 22:13:02 +0530

    PalindromeCheck

---
+++ src/PalindromeCheck.java
@@ -0,0 +1,18 @@
+public class PalindromeCheck {
+
+    public static boolean isPalindrome(String str) {
+        // Write your code here.
+        String rev = "";
+        for (int i = str.length()-1; i >= 0; i--) {
+            rev+=str.charAt(i);
+
+        }
+        return  rev==str;
+    }
+
+    public static void main(String[] args) {
+     //   System.out.println("Hello world!");
+        System.out.println( isPalindrome("hiih"));
+
+    }
+}
