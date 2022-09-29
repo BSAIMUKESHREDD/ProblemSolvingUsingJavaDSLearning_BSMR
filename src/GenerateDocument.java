390fa2b2bb904bc162d96f4e7133f2bbd59d6e6b
[bsaimukeshreddy] <bsaimukeshreddy143@gmail.com>
2022-09-29 18:29:36 +0530

    GenerateDocument

---
+++ src/GenerateDocument.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+
+public class GenerateDocument {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean generateDocument(String characters, String document) {
+     /*   // Write your code here.
+        int count=0;
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for (int i = 1; i < characters.length(); i++) {
+              Character a = characters.charAt(i);
+              if(characters.charAt(i)==characters.charAt(i-1)){
+                  count++;
+              }
+            hm.put(a,count);
+
+        }
+        return false;
+    }*/
+
+
+        HashMap<Character, Integer> characterCounts = new HashMap<Character, Integer>();
+        for (int idx = 0; idx < characters.length(); idx++) {
+            char character = characters.charAt(idx);
+            characterCounts.put(character, characterCounts.getOrDefault(character, 0) + 1);
+        }
+        for (int idx = 0; idx < document.length(); idx++) {
+            char character = document.charAt(idx);
+            if (!characterCounts.containsKey(character) || characterCounts.get(character) == 0) {
+                return false;
+            }
+            characterCounts.put(character, characterCounts.get(character) - 1);
+        }
+        return true;
+
+    }
+}
