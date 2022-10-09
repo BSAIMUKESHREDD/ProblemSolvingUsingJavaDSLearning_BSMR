465f053541be2eae0be8161f945a946f8f495182
[bsaimukeshreddy] <bsaimukeshreddy143@gmail.com>
2022-10-09 17:56:38 +0530

    ReverseWordsInAString

---
+++ src/ReverseWordsInAString.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseWordsInAString {
+    public static void main(String[] args) {
+        String input = "AlgoExpert is the best!";
+        String expected = "best! the is AlgoExpert";
+        String actual = reverseWordsInString(input);
+        System.out.println(expected==actual);
+    }
+
+    public static String reverseWordsInString(String string) {
+        // Write your code here.
+        List<String> ans = new ArrayList<>();
+        String sum = "";
+
+        for (int i = 0; i < string.length(); i++) {
+            Character searchforspace = string.charAt(i);
+
+            if(searchforspace ==' '){
+                ans.add(sum);
+                System.out.println(sum);
+
+                sum="";
+
+            }else {
+                sum  += string.charAt(i);
+
+            }
+
+
+        }
+        return "";
+    }
+}
