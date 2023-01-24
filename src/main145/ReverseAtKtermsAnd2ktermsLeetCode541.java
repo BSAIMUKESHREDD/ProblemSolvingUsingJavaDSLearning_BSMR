045f20879c3b4826452ce30a918db23881d1d30c
B SAI MUKESH REDDY <60388830+BSAIMUKESHREDD@users.noreply.github.com>
2023-01-24 22:19:52 +0530

    ReverseAtKtermsAnd2ktermsLeetCode541.java

---
+++ src/main145/ReverseAtKtermsAnd2ktermsLeetCode541.java
@@ -0,0 +1,28 @@
+package main145;
+
+public class ReverseAtKtermsAnd2ktermsLeetCode541 {
+
+    public static void main(String[] args) {
+        reverseStr("abcedfg",2);
+    }
+
+
+    public static String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int i = 0;
+        while(i < n) {
+            int j = Math.min(i + k - 1, n - 1);
+            swap(arr, i, j);
+            i += 2 * k;
+        }
+        return String.valueOf(arr);
+    }
+    private static void swap(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+    }
+}
